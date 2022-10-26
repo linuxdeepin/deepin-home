31e1b1c3367d9183c2182138d5fe670067f4773b
wurongjie <wurongjie@deepin.org>
2022-10-26 14:46:10 +0800

    feat: add system tray icon
    添加系统托盘功能
    
    Log: 显示系统托盘

--- src/maincomponentplugin/api/API.qml
+++ src/maincomponentplugin/api/API.qml
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 pragma Singleton
 import QtQuick 2.0
 import QtQuick.LocalStorage 2.0
--- src/maincomponentplugin/worker.cpp
+++ src/maincomponentplugin/worker.cpp
@@ -1,39 +1,40 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 #include "./worker.h"
 
 Worker::Worker(QObject *parent)
     : QObject(parent)
-{}
+{
+    daemon = new QDBusInterface("com.deepin.Home.Daemon",
+                                "/com/deepin/Home/Daemon",
+                                "com.deepin.Home.Daemon",
+                                QDBusConnection::sessionBus(),
+                                this);
+    if (daemon->isValid()) {
+        connect(daemon, SIGNAL(exit()), this, SLOT(exit()));
+    }
+}
 
 Worker::~Worker() {}
 
 QString Worker::getNode()
 {
     qDebug() << "get node";
-    QDBusInterface daemon("com.deepin.Home.Daemon",
-                          "/com/deepin/Home/Daemon",
-                          "com.deepin.Home.Daemon",
-                          QDBusConnection::sessionBus());
-    QDBusReply<QString> reply = daemon.call("getNode");
+    QDBusReply<QString> reply = daemon->call("getNode");
     return reply;
 };
 QString Worker::getLanguage()
 {
     qDebug() << "get language";
-    QDBusInterface daemon("com.deepin.Home.Daemon",
-                          "/com/deepin/Home/Daemon",
-                          "com.deepin.Home.Daemon",
-                          QDBusConnection::sessionBus());
-    QDBusReply<QString> reply = daemon.call("getLanguage");
+    QDBusReply<QString> reply = daemon->call("getLanguage");
     return reply;
 };
 QString Worker::getMachineID()
 {
     qDebug() << "get machine id";
-    QDBusInterface daemon("com.deepin.Home.Daemon",
-                          "/com/deepin/Home/Daemon",
-                          "com.deepin.Home.Daemon",
-                          QDBusConnection::sessionBus());
-    QDBusReply<QString> reply = daemon.call("getMachineID");
+    QDBusReply<QString> reply = daemon->call("getMachineID");
     return reply;
 };
 void Worker::markRead(QString channel, QString topic, QString uuid)
@@ -41,22 +42,13 @@ void Worker::markRead(QString channel, QString topic, QString uuid)
     qDebug() << "mark read";
     QList<QVariant> args;
     args << channel << topic << uuid;
-    QDBusInterface daemon("com.deepin.Home.Daemon",
-                          "/com/deepin/Home/Daemon",
-                          "com.deepin.Home.Daemon",
-                          QDBusConnection::sessionBus());
-
-    daemon.callWithArgumentList(QDBus::NoBlock, "markRead", args);
+    daemon->callWithArgumentList(QDBus::NoBlock, "markRead", args);
 };
 bool Worker::isRead(QString channel, QString topic, QString uuid)
 {
     qDebug() << "is read";
     QList<QVariant> args;
     args << channel << topic << uuid;
-    QDBusInterface daemon("com.deepin.Home.Daemon",
-                          "/com/deepin/Home/Daemon",
-                          "com.deepin.Home.Daemon",
-                          QDBusConnection::sessionBus());
-    QDBusReply<bool> reply = daemon.callWithArgumentList(QDBus::BlockWithGui, "isRead", args);
+    QDBusReply<bool> reply = daemon->callWithArgumentList(QDBus::BlockWithGui, "isRead", args);
     return reply;
 };
