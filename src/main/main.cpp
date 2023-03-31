700ed4e0f5877248c4992a46a2ef9d9d9804da68
myml <wurongjie@deepin.org>
2023-03-31 17:31:58 +0800

    fix: Modify appload's plugin path
    修改插件的标识信息，安装路径，查找路径
    
    Log:

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -17,13 +17,21 @@ int main(int argc, char *argv[])
         qDebug() << "DBus Error: register com.deepin.Home";
         return -1;
     }
+    // 亮色主题
     Dtk::Gui::DGuiApplicationHelper::instance()->setPaletteType(
         Dtk::Gui::DGuiApplicationHelper::ColorType::LightType);
-#ifdef LOCALLIBPATH
-    DAppLoader appLoader(APP_NAME, LOCALLIBPATH);
-#else
-    DAppLoader appLoader(APP_NAME);
-#endif
+    // 高分屏
     QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    DAppLoader appLoader(APP_NAME);
+#ifdef PLUGINPATH
+    appLoader.addPluginPath(PLUGINPATH);
+#endif
+#ifdef APP_PLUGIN_PATH
+    appLoader.addPluginPath(APP_PLUGIN_PATH);
+#endif
+#ifdef LOCALLIBPATH
+    appLoader.addPluginPath(LOCALLIBPATH);
+#endif
     return appLoader.exec(argc, argv);
 }
